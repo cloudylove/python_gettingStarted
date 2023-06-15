165b6a6068a836326affa8de4cdde6c68c1e2780
cloudylove <cloudylovechen@gmail.com>
2023-06-16 02:14:51 +0800

    canvas 畫球 with class & loop

--- canvas.py
+++ canvas.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 ###############
-# Canvas - 類別畫球
+# Canvas - 類別+迴圈畫球
 ###############
 
 # import
@@ -20,10 +20,10 @@ class Ball:
         print(self.x)
         print(self.y)
     def move(self, canvas):
-        canvas.create_oval(self.x-20, self.y-20, self.x+20, self.y+20, fill = "white", width = 0)
+        self.erase(canvas) #清除目前的圖形
         self.x = self.x + self.dx
         self.y = self.y + self.dy
-        canvas.create_oval(self.x-20, self.y-20, self.x+20, self.y+20, fill = self.color, width = 0)
+        self.draw(canvas)
         if self.x >= canvas.winfo_width():
             self.dx = -1
         if self.x <= 0:
@@ -32,12 +32,21 @@ class Ball:
             self.dy = -1
         if self.y <= 0:
             self.dy = +1
-# 帶入實例變數(instance variable)
-b = Ball(400, 300, 1, 1, "red")
-b.test()
+    def erase(self, canvas):
+        canvas.create_oval(self.x-20, self.y-20, self.x+20, self.y+20, fill = "white", width = 0)
+    def draw(self, canvas):
+        canvas.create_oval(self.x-20, self.y-20, self.x+20, self.y+20, fill = self.color, width = 0)
+
+# 串列包字典 # 圓型座標&移動量&顏色
+balls = [
+    Ball(0, 0, 1, 1, "red"),
+    Ball(120, 0, 1, 1, "blue"),
+    Ball(0, 350, 1, 1, "green")
+]
 
 def loop():
-    b.move(canvas) #移動
+    for i in balls:
+        i.move(canvas)
     root.after(10,loop) #0.01秒後重新執行
 
 # 描繪視窗
@@ -47,7 +56,6 @@ root.geometry("600x400")
 # Canvas
 canvas = tk.Canvas(root, width = 600, height = 400, bg = "white")
 canvas.place(x = 0, y = 0)
-
 
 # 設定計時器
 root.after(10, loop)
